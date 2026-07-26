98b611a291be40e484143dad98da067af0f05caa
littlezebrino <littlezebrino@gmail.com>
2026-07-26 16:23:35 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+async function loadBTC(){
 
+try{
+
+const response = await fetch("https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT");
+
+const data = await response.json();
+
+document.getElementById("btc-price").innerHTML="$"+Number(data.price).toLocaleString();
+
+}catch(error){
+
+document.getElementById("btc-price").innerHTML="API Error";
+
+}
+
+}
+
+loadBTC();
